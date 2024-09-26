baec4a01828d72198c29236703b9bca57ac5faf1
Mohamed20Ahmed <mohammedraslan942@gmail.com>
2024-09-26 23:45:44 +0300

    login

--- services/authService.js
+++ services/authService.js
@@ -1,16 +1,35 @@
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
+const ApiError = require("../utils/apiError");
 const User = require("../models/userModel");
+
+const createToken = (payload) =>
+  jwt.sign({ userId: payload }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRE_TIME,
+  });
 
 exports.signup = asyncHandler(async (req, res, next) => {
   const { name, slug, email, password } = req.body;
 
   const user = await User.create({ name, slug, email, password });
 
-  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
-    expiresIn: process.env.JWT_EXPIRE_TIME,
-  });
+  const token = createToken(user._id);
 
   res.status(201).json({ data: user, token });
+});
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return next(new ApiError("Incorrect email or password", 401));
+  }
+
+  const token = createToken(user._id);
+
+  res.status(200).json({ data: user, token });
 });
